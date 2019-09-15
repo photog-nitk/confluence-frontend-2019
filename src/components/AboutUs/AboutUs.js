c38820f1e77a830144bd2211dc64f1dca11aeb5e
Arpit Bhalla <arpitbhalla2001@gmail.com>
2019-09-15 17:40:38 +0530

    Add

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
-
+import {Row,Card,Col,Icon, CardTitle} from 'react-materialize'
+import 'AboutUs.css'
 class AboutUs extends Component {
-	
 	render() {
+		const users=[
+			{id:0,name:"Arpit",link:"sd"},
+			{id:1,name:"Arpait",link:"sd"}
+		];
 		return (
 			<div>
-
-				<h1>AboutUs is here</h1>
-
+<Row>
+{users.map(user=>(
+<Col l={2} m={3} s={12}>
+<Card title={user.name} header={<img src="http://arpit.rf.gd/profile.png"/>}>
+	fsd
+</Card>
+</Col>
+))}
+</Row>
 			</div>
 		);
 	}

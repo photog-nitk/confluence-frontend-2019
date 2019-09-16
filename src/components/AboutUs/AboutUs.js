e70350840b70eb42809c3532248d2febde96e622
Arpit Bhalla <arpitbhalla2001@gmail.com>
2019-09-16 19:46:30 +0530

    Developers Page

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -1,27 +1,17 @@
 import React, { Component } from 'react';
-import {Row,Card,Col,Icon, CardTitle} from 'react-materialize'
-import 'AboutUs.css'
+
 class AboutUs extends Component {
+	
 	render() {
-		const users=[
-			{id:0,name:"Arpit",link:"sd"},
-			{id:1,name:"Arpait",link:"sd"}
-		];
 		return (
 			<div>
-<Row>
-{users.map(user=>(
-<Col l={2} m={3} s={12}>
-<Card title={user.name} header={<img src="http://arpit.rf.gd/profile.png"/>}>
-	fsd
-</Card>
-</Col>
-))}
-</Row>
+
+				<h1>AboutUs is here</h1>
+
 			</div>
 		);
 	}
 }
 
-export default AboutUs;
+export default HomePage;
 	
---
+++ src/components/Developers/Developers.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import {Row,Container,Col} from 'react-materialize'
+import './Developers.css'
+
+class Developers extends Component {
+	render() {
+		const users=[
+			{id:0,name:"User",img:"https://ui-avatars.com/api/?size=216&name=User",link:[{name:"whatsapp",url:""},{name:"facebook",url:""},{name:"twitter",url:""}]},
+			{id:1,name:"User",img:"https://ui-avatars.com/api/?size=216&name=User",link:[{name:"whatsapp",url:""},{name:"linkedin",url:""},{name:"snapchat",url:""}]},
+			{id:2,name:"User",img:"https://ui-avatars.com/api/?size=216&name=User",link:[{name:"whatsapp",url:""},{name:"instagram",url:""},{name:"quora",url:""},{name:"github",url:""}]},
+		];
+		const colors=[
+			{init:"D21DB0",final:"9133BF"},
+			{init:"FFB707",final:"FF6401"},
+			{init:"00FDFD",final:"00C6FC"}
+		]
+		return (
+			<React.Fragment>
+				<center>	
+					<h1>Our Team</h1>
+					</center>
+				<Container>
+<Row>
+{users.map((user,i)=>(
+<Col l={4} m={4} s={12}>
+<div className='card'>
+	<div className='show' style={{backgroundImage:'linear-gradient(45deg,#'+ colors[user.id % colors.length].init +', #'+ colors[user.id % colors.length].final +')'}}></div>
+	<div className='card-content'>
+		<img src={user.img}/>
+          <h5 style={{color:'#'+colors[user.id % colors.length].init}}>{user.name}</h5>
+		  <div className='social'>
+			  {user.link.map((link,ii)=>(
+			 <a href={link.url} className={'fab fa-' + link.name }></a>
+			  ))}
+
+		  </div>
+		  </div>
+</div>
+
+</Col>
+))}
+</Row></Container>
+</React.Fragment>
+		);
+	}
+}
+
+export default Developers;
+	

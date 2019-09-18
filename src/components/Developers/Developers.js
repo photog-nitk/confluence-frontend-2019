823c8214b602e00f5cf6b5f0cb15fd062787142b
Arpit Bhalla <55053424+arpitBhalla@users.noreply.github.com>
2019-09-18 22:08:36 +0530

    Update Developers.js

--- src/components/Developers/Developers.js
+++ src/components/Developers/Developers.js
@@ -4,11 +4,14 @@ import './Developers.css'
 
 class Developers extends Component {
 	render() {
+		//Const or fetch API
 		const users=[
 			{id:0,name:"User",img:"https://ui-avatars.com/api/?size=216&name=User",link:[{name:"whatsapp",url:""},{name:"facebook",url:""},{name:"twitter",url:""}]},
 			{id:1,name:"User",img:"https://ui-avatars.com/api/?size=216&name=User",link:[{name:"whatsapp",url:""},{name:"linkedin",url:""},{name:"snapchat",url:""}]},
 			{id:2,name:"User",img:"https://ui-avatars.com/api/?size=216&name=User",link:[{name:"whatsapp",url:""},{name:"instagram",url:""},{name:"quora",url:""},{name:"github",url:""}]},
 		];
+		
+		//For linear Gradiant
 		const colors=[
 			{init:"D21DB0",final:"9133BF"},
 			{init:"FFB707",final:"FF6401"},
